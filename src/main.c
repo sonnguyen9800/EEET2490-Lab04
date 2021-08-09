de0f818b1986c699037ae80b880fc3e4a698ecc6
Son Nguyen Hoang <sonnguyen9800@gmail.com>
2021-08-09 16:20:58 +0700

    Update

--- src/main.c
+++ src/main.c
@@ -1,105 +1,30 @@
-#include "mbox.h"
 #include "uart.h"
+#include "mbox.h"
+#include "framebf.h"
 
 
 void main()
 {
     // set up serial console
 	uart_init();
-
-	// say hello
 	uart_puts("Hello World!\n");
 
-	// mailbox data buffer: Read ARM frequency
-	mbox[0] = 8*4;          // Message Buffer Size in bytes (8 elements * 4 bytes (32 bit) each)
-	mbox[1] = MBOX_REQUEST; // Message Request Code (this is a request message)
-	mbox[2] = 0x00030002;   // TAG Identifier: Get clock rate
-	mbox[3] = 8;            // Value buffer size in bytes (max of request and response lengths)
-	mbox[4] = 0;			// REQUEST CODE = 0
-	mbox[5] = 3;			// clock id: ARM system clock
-	mbox[6] = 0;            // clear output buffer (response data are mbox[5] & mbox[6])
-	mbox[7] = MBOX_TAG_LAST;
+	framebf_init();
 
-	if (mbox_call(ADDR(mbox), MBOX_CH_PROP)) {
-		uart_puts("Response Code for whole message: ");
-		uart_hex(mbox[1]);
-		uart_puts("\n");
+	// say hello
 
-		uart_puts("Response Code in Message TAG: ");
-		uart_hex(mbox[4]);
-		uart_puts("\n");
-
-		uart_puts("DATA: ARM clock rate = ");
-		uart_dec(mbox[6]);
-		uart_puts("\n\n");
-	} else {
-		uart_puts("Unable to query!\n");
+	drawRectARGB32(100,100,400,400,0x00AA0000,1); //RED
+	drawRectARGB32(150,150,400,400,0x0000BB00,1); //GREEN
+	drawRectARGB32(200,200,400,400,0x000000CC,1); //BLUE
+	drawRectARGB32(250,250,400,400,0x00FFFF00,1); //YELLOW
+	drawPixelARGB32(300, 300, 0x00FF0000); //RED
+	// echo everything back
+	while(1) {
+	//read each char
+	char c = uart_getc();
+	//send back twice
+	uart_sendc(c);
+	uart_sendc(c);
 	}
 
-
-
-	// mailbox data buffer: Read ARM frequency
-	mbox[0] = 8*4;          // Message Buffer Size in bytes (8 elements * 4 bytes (32 bit) each)
-	mbox[1] = MBOX_REQUEST; // Message Request Code (this is a request message)
-	mbox[2] = 0x00030002;   // TAG Identifier: Get clock rate
-	mbox[3] = 8;            // Value buffer size in bytes (max of request and response lengths)
-	mbox[4] = 0;			// REQUEST CODE = 0
-	mbox[5] = 2;			// clock id: UART clock rate
-	mbox[6] = 0;            // clear output buffer (response data are mbox[5] & mbox[6])
-	mbox[7] = MBOX_TAG_LAST;
-
-	if (mbox_call(ADDR(mbox), MBOX_CH_PROP)) {
-		uart_puts("Response Code for whole message: ");
-		uart_hex(mbox[1]);
-		uart_puts("\n");
-
-		uart_puts("Response Code in Message TAG: ");
-		uart_hex(mbox[4]);
-		uart_puts("\n");
-
-		uart_puts("DATA: UART clock rate = ");
-		uart_dec(mbox[6]);
-		uart_puts("\n\n");
-	} else {
-		uart_puts("Unable to query!\n");
-	}
-
-
-	//Read Board Revision
-	mbox[0] = 7*4;          // Message Buffer Size in bytes (9 elements * 4 bytes (32 bit) each)
-	mbox[1] = MBOX_REQUEST; // Message Request Code (this is a request message)
-
-	mbox[2] = 0x00010002;   // TAG Identifier: Get clock rate
-	mbox[3] = 4;            // Value buffer size in bytes (max of request and response lengths)
-	mbox[4] = 0;			// REQUEST CODE = 0
-	mbox[5] = 0;            // clear output buffer
-	mbox[6] = MBOX_TAG_LAST;
-
-	if (mbox_call(ADDR(mbox), MBOX_CH_PROP)) {
-		uart_puts("Response Code for whole message: ");
-		uart_hex(mbox[1]);
-		uart_puts("\n");
-
-		uart_puts("Response Code in Message TAG: ");
-		uart_hex(mbox[4]);
-		uart_puts("\n");
-
-		uart_puts("DATA: board revision = ");
-		uart_hex(mbox[5]);
-		uart_puts("\n\n");
-
-	} else {
-		uart_puts("Unable to query!\n");
-	}
-
-
-    // echo everything back
-    while(1) {
-    	//read each char
-    	char c = uart_getc();
-
-    	//send back twice
-    	uart_sendc(c);
-    	uart_sendc(c);
-    }
 }
--- src/mbox.h
+++ src/mbox.h
@@ -1,5 +1,14 @@
 #include "gpio.h"
 
+//New Tags for Screen Display
+#define MBOX_TAG_SETPHYWH 0x48003
+#define MBOX_TAG_SETVIRTWH 0x48004
+#define MBOX_TAG_SETVIRTOFF 0x48009
+#define MBOX_TAG_SETDEPTH 0x48005
+#define MBOX_TAG_SETPXLORDR 0x48006
+#define MBOX_TAG_GETFB 0x40001
+#define MBOX_TAG_GETPITCH 0x40008
+
 /* a properly aligned buffer */
 extern volatile unsigned int mbox[36];
 #define ADDR(X) (unsigned int)((unsigned long) X)
